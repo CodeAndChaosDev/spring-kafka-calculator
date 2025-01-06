c82899d1190377974b91bece29acca3e3ffb735f
CodeAndChaosDev <brunosousa.software@gmail.com>
2025-01-06 14:28:29 +0000

    First Commit

---
+++ Calculator-API/src/test/java/com/project/calculator_api/Calculator/API/CalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.calculator_api.Calculator.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
