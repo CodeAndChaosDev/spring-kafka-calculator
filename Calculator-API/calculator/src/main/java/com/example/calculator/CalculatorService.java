5b86e3155c2442c1e0cb2dbb0e31f77d8240588d
CodeAndChaosDev <brunosousa.software@gmail.com>
2025-01-06 16:26:26 +0000

    +feat(multi modules integration)

---
+++ Calculator-API/calculator/src/main/java/com/example/calculator/CalculatorService.java
@@ -0,0 +1,25 @@
+package com.example.calculator; 
+
+import java.math.BigDecimal;
+
+public class CalculatorService {
+    public BigDecimal sum(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+
+    public BigDecimal subtract(BigDecimal a, BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    public BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        return a.multiply(b);
+    }
+
+    public BigDecimal divide(BigDecimal a, BigDecimal b) {
+        if (b.compareTo(BigDecimal.ZERO) == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a.divide(b);
+    }
+
+}
